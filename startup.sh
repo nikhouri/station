fbfaae7ddc03a778477b92f4fbfb08afa7cc115e
nik <nik@nikhouri.com>
2021-10-04 01:17:52 +0100

    DEVOPS: Working startup script, kdb+tick scripts launched in tmux

--- startup.sh
+++ startup.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+# From https://how-to.dev/how-to-create-tmux-session-with-a-script
 
-# Start tickerplant, RDB, HDB
-q ~/q/tick.q sym ~/observatory/tick -p 5010
-q tick/hdb.q ~/observatory/tick/sym -p 5012
-q ~/q/tick/r.q localhost:5000 localhost:5002 -p 5011
+session="observatory_tick"
+tmux new-session -d -s $session
+
+# Tickerplant
+window=0
+tmux rename-window -t $session:$window 'tick'
+tmux send-keys -t $session:$window 'q tick.q db tick -p 5010' C-m
+
+# HDB
+window=1
+tmux new-window -t $session:$window -n 'hdb'
+tmux send-keys -t $session:$window 'q tick/hdb.q tick/db -p 5012' C-m
+
+# RDB
+window=2
+tmux new-window -t $session:$window -n 'rdb'
+tmux send-keys -t $session:$window 'q tick/r.q localhost:5010 localhost:5012 -p 5011' C-m
