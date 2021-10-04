9063ae08c3a11ad61610884aa2e43f1e43711f97
nik <nik@nikhouri.com>
2021-10-04 01:51:27 +0100

    FEAT: Added handling for standard computers (no environmental chips)

--- sample.py
+++ sample.py
@@ -4,10 +4,17 @@ import requests
 import socket
 import psutil
 from datetime import datetime
-from smbus2 import SMBus
-from bme280 import BME280
-from mics6814 import MICS6814
-
+try:
+    from smbus2 import SMBus
+    from bme280 import BME280
+    BMEPRESENT = True
+except ImportError:
+    BMEPRESENT = False
+try:
+    from mics6814 import MICS6814
+    MICSPRESENT = True
+except ImportError:
+    MICSPRESENT = False
 
 LOOPSECS = 30
 TRANSMITSECS = 5
@@ -25,11 +32,11 @@ def ts():
     """Make a kdb timespan for UTC now"""
     return datetime.utcnow().strftime("0D%H:%M:%S.%f")
 
-def upd(meas):
+def upd(data):
     """Send an update to the database"""
-    mts, mtype, munit, measure = meas
-    query = '.u.upd[`obs;(' + str(mts) + ';' + str(mtype) + ';' + str(munit) + \
-        ';' + str(measure) + ')]'
+    ts, sym, host, units, data = data
+    query = '.u.upd[`obs;(' + str(ts) + ';`' + str(sym) + ';`' + str(host) + \
+        ';`' + str(units) + ';' + str(data) + ')]'
     try:
         r = requests.get(PROTO + DBHOST + PORT + PATH + query)
         return r.status_code
@@ -39,28 +46,34 @@ def upd(meas):
 if __name__ == '__main__':
 
     # Initialise the BME280 & MICS6814 breakouts, disable LEDs
-    bus = SMBus(1)
-    bme280 = BME280(i2c_dev=bus)
-    gas = MICS6814()
-    gas.set_brightness(0.0)
-    gas.set_led(0, 0, 0)
+    if BMEPRESENT:
+        bus = SMBus(1)
+        bme280 = BME280(i2c_dev=bus)
+    if MICSPRESENT:
+        gas = MICS6814()
+        gas.set_brightness(0.0)
+        gas.set_led(0, 0, 0)
 
     # Load any saved stashed values
+    # TODO
 
     # Loop to keep sampling & sending to the DB
     while True:
 
         # Take measurements & stash them - make these strings??
-        STASH.append([ts(),HOST,'`temperature','`C',bme280.get_temperature()])
-        STASH.append([ts(),HOST,'`pressure','`hPa',bme280.get_pressure()])
-        STASH.append([ts(),HOST,'`humidity','`pct',bme280.get_humidity()])
-        STASH.append([ts(),HOST,'`oxidising','`Ohms',gas.read_oxidising()])
-        STASH.append([ts(),HOST,'`reducing','`Ohms',gas.read_reducing()])
-        STASH.append([ts(),HOST,'`nh3','`Ohms',gas.read_nh3()])
-        STASH.append([ts(),HOST,'`cpu','`pct',psutil.cpu_percent(percpu = True)])
-        STASH.append([ts(),HOST,'`cputemp','`C',psutil.sensors_temperatures()['cpu_thermal'][0].current])
-        STASH.append([ts(),HOST,'`mem','`pct',psutil.virtual_memory().percent])
-        #print(str(STASH[-9:]))
+        if BMEPRESENT: # If we have the BME chip, sample data
+            STASH.append([ts(),'temperature',HOST,'C',bme280.get_temperature()])
+            STASH.append([ts(),'pressure',HOST,'hPa',bme280.get_pressure()])
+            STASH.append([ts(),'humidity',HOST,'pct',bme280.get_humidity()])
+        if MICSPRESENT: # If we have the MICS chip, sample data
+            STASH.append([ts(),'oxidising',HOST,'Ohms',gas.read_oxidising()])
+            STASH.append([ts(),'reducing',HOST,'Ohms',gas.read_reducing()])
+            STASH.append([ts(),'nh3',HOST,'Ohms',gas.read_nh3()])
+        # Sample generic host load data
+        STASH.append([ts(),'cpu',HOST,'pct',psutil.cpu_percent(percpu = False)])
+        STASH.append([ts(),'cputemp',HOST,'C',psutil.sensors_temperatures()['cpu_thermal'][0].current])
+        STASH.append([ts(),'mem',HOST,'pct',psutil.virtual_memory().percent])
+        #print(str(STASH[-9:])) # Print our last samples
 
         # Try to transmit stashed data (and SOME of the last updates -
         #    so ex. generate 6 every time, try to send 30)
@@ -68,10 +81,10 @@ if __name__ == '__main__':
         # TODO: move to thread? will lock up otherwise while sending
         # TODO: every STASHSAVESECS, persist to disk
         TEMPSTASH = []
-        for meas in STASH:
-            response = upd(meas)
-            if (response != 200): # Failed, put it back into the stash
-                TEMPSTASH.append(meas)
+        for obs in STASH:
+            response = upd(obs)
+            if (response != 200): # Failed, add it back into the stash
+                TEMPSTASH.append(obs)
         STASH = TEMPSTASH
 
         time.sleep(LOOPSECS)
