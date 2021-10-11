47977651df9b28215fa716802c1db7dc03a89db1
nik <nik@nikhouri.com>
2021-10-11 01:24:27 +0100

    FIX: Publisher cron offset by 1 minute (avoid missing 10-minute medians)

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 # Crontab entry:
-# */5 * * * * timeout 1m /home/nik/observatory/publish.sh > /home/nik/observatory/publish.out 2>&1
+# 1/5 * * * * timeout 1m /home/nik/observatory/publish.sh > /home/nik/observatory/publish.out 2>&1
 cd /home/nik/observatory/
 Rscript -e "library(rmarkdown);rmarkdown::render('obs.Rmd')"
 scp -P 42024 obs.html nik@nikhouri.com:/var/www/html/o.nikhouri.com/index.html
